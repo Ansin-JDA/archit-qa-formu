996a7ebadd9726e4e1ac2d52dd65d910bcea403c
Ansin.Wang <ansin.wang@gmail.com>
2015-07-16 22:29:36 +0800

    Implemented user, Manager, Question and answer in placeholder

--- src/main/java/com/knowledge/infra/server/placeholder.java
+++ src/main/java/com/knowledge/infra/server/placeholder.java
@@ -7,6 +7,11 @@ import com.knowledge.infra.server.dao.UserDao;
 import com.knowledge.infra.server.model.User;
 import com.knowledge.infra.server.dao.QuestionDao;
 import com.knowledge.infra.server.model.Question;
+import com.knowledge.infra.server.dao.AnswerDao;
+import com.knowledge.infra.server.model.Answer;
+import com.knowledge.infra.server.dao.ManagerDao;
+import com.knowledge.infra.server.model.Manager;
+
 
 //两步命令来执行这儿的main函数
 //Command: mvn clean compile exec:java -Dexec.mainClass="com.knowledge.infra.server.placeholder"
@@ -71,16 +76,83 @@ public class placeholder {
 	        question.setQlabel("question 2");
 	        questionDao.addQuestion(question);
 	        System.out.println("添加测试问题成功");
-		
-		}catch(Exception e){
-			
-			e.printStackTrace();
-			
-		}
-		
-		
-	}
-	
-	
-	
+	        
+	        //查询数据 TODO
+	        
+	        //修改数据
+	        question.setQuestionid(2);
+	        question.setQtitle("Title After updating - 2");
+	        questionDao.updateQuestion(question);
+	        System.out.println("修改测试问题成功");
+	        //删除数据
+	        questionDao.deleteQuestion(1);
+	        questionDao.deleteQuestion(2);
+	        System.out.println("测除测试问题成功");
+	        
+	        
+	        System.out.println("=================Main: Test Answer part begins!!!==================");
+	        AnswerDao answerDao=(AnswerDao) ctx.getBean("answerDao");
+		    Answer answer=new Answer();
+		    //添加两条数据
+		    answer.setAreferenceqid(1);
+		    answer.setAtitle("testAnswer 1");
+		    answer.setAcontent("testContent 1");
+		    answer.setAcreateuserid(5);
+		    answer.setAupdateuserid(6);
+		    answerDao.addAnswer(answer);
+		    answer.setAreferenceqid(2);
+		    answer.setAtitle("testAnswer 2");
+		    answer.setAcontent("testContent 2");
+		    answer.setAcreateuserid(7);
+		    answer.setAupdateuserid(8);
+		    answerDao.addAnswer(answer);
+		    System.out.println("添加测试回答成功");
+		    
+		    //查询数据   TODO
+		    
+		    //修改数据 (暂时还没有关于goodnum和badnum的更改！)
+		    answer.setAnswerid(2);
+		    answer.setAtitle("Title after updating - 2");
+		    answer.setAcontent("Content after updating - 2");
+		    answerDao.updateAnswer(answer);
+		    System.out.println("修改测试回答成功");
+		    
+		    //删除数据
+		    answerDao.deleteAnswer(1);
+		    answerDao.deleteAnswer(2);
+		    System.out.println("测除测试数据成功");
+		    
+		    
+		    System.out.println("=================Main: Test Manager part begins!!!==================");
+		    ManagerDao managerDao=(ManagerDao) ctx.getBean("managerDao");
+		    Manager manager=new Manager();
+		    //添加两条数据
+		    manager.setMname("test Manager 1");
+		    manager.setMemail("testManager1@zwvalley.com");
+		    manager.setMpassword("123456");
+		    managerDao.addManager(manager);
+		    manager.setMname("test Manager 2");
+		    manager.setMemail("testManager2@zwvalley.com");
+		    manager.setMpassword("123");
+		    managerDao.addManager(manager);
+		    System.out.println("添加测试Managers成功");
+		    
+		    //查询数据 TODO
+		    
+		    //修改数据
+		    manager.setManagerid(2);
+		    manager.setMemail("updateMail@zwvalley.com");
+		    manager.setMpassword("123456");
+		    managerDao.updateManager(manager);
+		    System.out.println("修改测试Manager成功");
+		    
+		    //删除数据
+		    managerDao.deleteManager(1);
+		    managerDao.deleteManager(2);
+		    System.out.println("删除测试Manager成功");		    
+		    
+		}catch(Exception e){			
+			e.printStackTrace();			
+		}		
+	}	
 }
